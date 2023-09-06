3f1ef1affededee9c0f73e80c728c4483d2a2433
Lerato <lerato.ntsoereng@entelect.co.za>
2023-09-06 14:21:09 +0200

    Initial commit

---
+++ src/main/java/za/co/msrv/incubator/MsrvIncubatorApplication.java
@@ -0,0 +1,13 @@
+package za.co.msrv.incubator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsrvIncubatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsrvIncubatorApplication.class, args);
+    }
+
+}
