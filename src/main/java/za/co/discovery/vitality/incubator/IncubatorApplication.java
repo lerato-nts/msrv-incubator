f1d14a8946352ff746802d1130163c79a6ffb10b
Lerato <137291592+lerato-nts@users.noreply.github.com>
2023-09-06 12:21:30 +0200

    Hello world API accepting a Get and a POST request
    API Requests get be made using Swagger.
    The GET request returns a success message: "Hello World!"
    The POST request returns the user message sent with the request body.

---
+++ src/main/java/za/co/discovery/vitality/incubator/IncubatorApplication.java
@@ -0,0 +1,12 @@
+package za.co.discovery.vitality.incubator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncubatorApplication {
+
+        public static void main(String[] args) {
+            SpringApplication.run(IncubatorApplication.class, args);
+        }
+}
---
+++ src/main/java/za/co/discovery/vitality/incubator/controller/HelloWorldController.java
@@ -0,0 +1,31 @@
+package za.co.discovery.vitality.incubator.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Api(value = "Hello World Controller")
+@RequestMapping("/api/v1/hello-world")
+public class HelloWorldController {
+    private final String successMessage;
+
+    public HelloWorldController(String successMessage) {
+        this.successMessage = successMessage;
+    }
+
+    @GetMapping
+    @ApiOperation(value = "Says Hello")
+    public ResponseEntity<String> sayHello() {
+        return ResponseEntity.ok(successMessage);
+    }
+
+    @PostMapping("/message")
+    @ApiOperation(value = "Sends back the message from the user")
+    public ResponseEntity<String> sendMessage(@ApiParam(value = "The message to send to the API")
+                                                  @RequestBody String userMessage) {
+        return ResponseEntity.ok(userMessage);
+    }
+}
