ab8962274ae36668a233501e4ce2debb1ceef217
Lerato <lerato.ntsoereng@entelect.co.za>
2023-09-07 14:55:44 +0200

    Super hero classes used to store the hero's details

--- src/main/java/za/co/msrv/incubator/model/SuperHeroBiography.java
+++ src/main/java/za/co/msrv/incubator/model/SuperHeroBiography.java
@@ -1,13 +1,8 @@
 package za.co.msrv.incubator.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-//@NoArgsConstructor
-//@AllArgsConstructor
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SuperHeroBiography {
