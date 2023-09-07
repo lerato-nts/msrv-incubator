6f508be1eee88b8ebfd290a3f57e4ad9e8d70fa7
Lerato <lerato.ntsoereng@entelect.co.za>
2023-09-07 14:59:55 +0200

    Mapper class to convert the Super hero entity to a dto

---
+++ src/main/java/za/co/msrv/incubator/service/SuperHeroMapper.java
@@ -0,0 +1,16 @@
+package za.co.msrv.incubator.service;
+
+import org.springframework.stereotype.Service;
+import za.co.msrv.incubator.dto.SuperHeroDTO;
+import za.co.msrv.incubator.model.SuperHero;
+
+@Service
+public class SuperHeroMapper {
+    public SuperHeroDTO convertToDto(SuperHero superHero) {
+        return SuperHeroDTO.builder()
+                            .title(superHero.getName())
+                            .paragraphText(superHero.getBiography().toString())
+                            .imageUrl(superHero.getImages().getMd())
+                                    .build();
+    }
+}
