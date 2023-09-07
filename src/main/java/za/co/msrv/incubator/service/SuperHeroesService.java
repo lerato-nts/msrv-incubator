6d93bbe5243709baf1c7495bceb59b74e53243a8
Lerato <lerato.ntsoereng@entelect.co.za>
2023-09-07 15:01:35 +0200

    Service calling the Super Heroes external API

--- src/main/java/za/co/msrv/incubator/service/SuperHeroesService.java
+++ src/main/java/za/co/msrv/incubator/service/SuperHeroesService.java
@@ -1,5 +1,8 @@
 package za.co.msrv.incubator.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -7,6 +10,13 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import za.co.msrv.incubator.dto.SuperHeroDTO;
+import za.co.msrv.incubator.model.SuperHero;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 public class SuperHeroesService {
@@ -22,13 +32,18 @@ public class SuperHeroesService {
         this.restTemplate = restTemplateBuilder.build();
     }
 
-    public String getSuperHeroList() {
+    public List<SuperHero> getSuperHeroList() throws JsonProcessingException {
         HttpHeaders headers = new HttpHeaders();
         headers.set(API_HEADER_KEY, API_HEADER_KEY_VALUE);
         headers.set(API_HEADER_HOST, API_HEADER_HOST_VALUE);
+
         HttpEntity<String> entity = new HttpEntity<>(headers);
         ResponseEntity<String> response = restTemplate.exchange(API_URL, HttpMethod.GET, entity, String.class);
 
-        return response.getBody();
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        return objectMapper.readValue(
+                response.getBody(),
+                new TypeReference<List<SuperHero>>(){});
     }
 }
