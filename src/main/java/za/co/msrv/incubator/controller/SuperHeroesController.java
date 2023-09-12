34f8c082f2f5ce97f37dade66c1d177c1ec54de5
Lerato Ntsoereng <lerato.ntsoereng@entelect.co.za>
2023-09-12 08:33:48 +0200

    Update the controller to have query params for filtering

--- src/main/java/za/co/msrv/incubator/controller/SuperHeroesController.java
+++ src/main/java/za/co/msrv/incubator/controller/SuperHeroesController.java
@@ -1,6 +1,5 @@
 package za.co.msrv.incubator.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +12,7 @@ import za.co.msrv.incubator.model.SuperHero;
 import za.co.msrv.incubator.service.IHeroesService;
 import za.co.msrv.incubator.service.SuperHeroMapper;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,9 +30,12 @@ public class SuperHeroesController {
     }
 
     @GetMapping
-    @ApiOperation(value = "A list of 20 random Super Heroes")
-    public ResponseEntity<List<SuperHeroDTO>> getSuperHeroes() throws JsonProcessingException {
-        List<SuperHero> superHeroList = superHeroesService.getSuperHeroList();
+    @ApiOperation(value = "A list of Super Heroes matching the search parameter")
+    public ResponseEntity<List<SuperHeroDTO>> getHeroesByFilter(
+            @PathParam("search") String searchPhrase,
+            @PathParam("size") int resultSize)
+    {
+        List<SuperHero> superHeroList = superHeroesService.getSuperHeroByFilter(searchPhrase, resultSize);
 
         List<SuperHeroDTO> superHeroDTOList = superHeroList.stream()
                 .map(superHeroMapper::convertToDto)
